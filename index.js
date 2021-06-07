ba309c7f3cc8b961b306091b16a0a8146b6bb52c
sekthor <sysdroyed@gmail.com>
2021-06-07 17:08:12 +0200

    added post methods for adjectives and nouns

--- index.js
+++ index.js
@@ -1,8 +1,20 @@
 const express = require("express");
 const app = express();
+const fs = require("fs");
+
+app.use(express.json())
 
 var nouns = require("./data/nouns.json");
 var adjectives = require("./data/adjectives.json");
+
+function saveListToFile(list, file) {
+    list.sort();
+    fs.writeFile(file, JSON.stringify(list), "utf8", (err) => {
+        if (err) {
+            console.log("[!] error writing file");
+        }
+    });
+}
 
 function randomChoice(list) {
     return list[Math.floor(Math.random() * list.length)];
@@ -15,4 +27,17 @@ app.get("/name", (req, res) => {
     res.send({ "name": adj + "-" + noun });
 });
 
-app.listen(3000, () => console.log("Listening on port 3000"))
+app.post("/adjective", (req, res) => {
+    adjectives.push(req.body.adjective);
+    saveListToFile(adjectives, "./data/adjectives.json");
+    res.send(req.body);
+});
+
+app.post("/noun", (req, res) => {
+    adjectives.push(req.body.adjective);
+    saveListToFile(adjectives, "./data/nouns.json");
+    res.send(req.body);
+})
+
+const port = process.env.PORT || 3000
+app.listen(port, () => console.log(`Listening on port ${port}`))
